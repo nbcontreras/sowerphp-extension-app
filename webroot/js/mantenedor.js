3940bcaec56e92f462a441b9ce1387a9134df4d6
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-04-13 22:01:24 -0300

    Corregido código según JSLint y nuevas clases __ y Form

--- webroot/js/mantenedor.js
+++ webroot/js/mantenedor.js
@@ -19,29 +19,33 @@
  * En caso contrario, consulte <http://www.gnu.org/licenses/gpl.html>.
  */
 
+/*jslint browser: true, devel: true, nomen: true */
+
 /**
  * Envía un formulario para filtrar por diferentes parámetros
  * @param formulario Formulario genérico que se utilizará para enviar elementos
  * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
- * @version 2011-03-29
+ * @version 2014-04-13
  */
-function buscar (formulario) {
-	var total = formulario.elements.length;
-	var search = new Array();
-	for (i=0; i<total; ++i) {
-		campo = formulario.elements[i].name;
-		valor = formulario.elements[i].value;
-		if(!Form.empty(valor)) search.push(campo+':'+valor);
-	}
-	search = search.join(",");
-	if(Form.empty(search))
-		document.location.href = window.location.pathname;
-	else
-		document.location.href =
-			window.location.pathname + '?search=' + search;
-	// en teoria nunca se llega aquí, pero está para que no reclame porque
-	// buscar() no retorna nada
-	return false;
+function buscar(formulario) {
+    'use strict';
+    var i, total = formulario.elements.length, search = [], campo, valor;
+    for (i = 0; i < total; i += 1) {
+        campo = formulario.elements[i].name;
+        valor = formulario.elements[i].value;
+        if (!__.empty(valor)) {
+            search.push(campo + ':' + valor);
+        }
+    }
+    search = search.join(",");
+    if (__.empty(search)) {
+        document.location.href = window.location.pathname;
+    } else {
+        document.location.href = window.location.pathname + '?search=' + search;
+    }
+    // en teoria nunca se llega aquí, pero está para que no reclame porque
+    // buscar() no retorna nada
+    return false;
 }
 
 /**
@@ -49,8 +53,9 @@ function buscar (formulario) {
  * @param registro Nombre del registro
  * @param tupla Identificador (generalmente PK) del registro
  * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
- * @version 2014-03-16
+ * @version 2014-04-13
  */
-function eliminar (registro, tupla) {
-	return confirm('¿Eliminar registro '+registro+'('+tupla+')?')
+function eliminar(registro, tupla) {
+    'use strict';
+    return confirm('¿Eliminar registro ' + registro + '(' + tupla + ')?');
 }
