6f68cbe974ff473cbb96eb841d08c7e5fb0b9ad1
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-09-10 14:09:39 -0300

    Módulo Sistema.Usuarios: Se corrije script para MariaDB

--- Module/Sistema/Module/Usuarios/Model/Sql/MariaDB/usuarios.sql
+++ Module/Sistema/Module/Usuarios/Model/Sql/MariaDB/usuarios.sql
@@ -1,22 +1,34 @@
-﻿-- SCHEMA PARA MÓDULO: Sistema.Usuarios
+﻿-- SCRIPT SQL PARA MÓDULO: Sistema.Usuarios
 -- Base de datos: MariaDB
--- WARNING: no se están usando llaves foráneas: ¿ERROR 1005 (errno: 150)?
 
-START TRANSACTION;
+BEGIN;
+
+-- SCHEMA PARA EL MÓDULO: Sistema.Usuarios
+SET FOREIGN_KEY_CHECKS=0;
 
 -- tabla para usuarios
 DROP TABLE IF EXISTS usuario CASCADE;
 CREATE TABLE usuario (
-    id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Identificador (serial)',
-    nombre CHARACTER VARYING (50) NOT NULL COMMENT 'Nombre real del usuario',
-    usuario CHARACTER VARYING (30) NOT NULL COMMENT 'Nombre de usuario',
-    email CHARACTER VARYING (50) NOT NULL COMMENT 'Correo electrónico del usuario',
-    contrasenia CHAR(64) NOT NULL COMMENT 'Contraseña del usuario',
-    hash CHAR(32) NOT NULL COMMENT 'Hash único del usuario (32 caracteres)',
-    activo BOOLEAN NOT NULL DEFAULT true COMMENT 'Indica si el usuario está o no activo en la aplicación',
-    ultimo_ingreso_fecha_hora TIMESTAMP COMMENT 'Fecha y hora del último ingreso del usuario',
-    ultimo_ingreso_desde CHARACTER VARYING (45) COMMENT 'Dirección IP del último ingreso del usuario',
-    ultimo_ingreso_hash CHAR(32) COMMENT 'Hash del último ingreso del usuario'
+	id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
+		COMMENT 'Identificador (serial)',
+	nombre CHARACTER VARYING (50) NOT NULL
+		COMMENT 'Nombre real del usuario',
+	usuario CHARACTER VARYING (30) NOT NULL
+		COMMENT 'Nombre de usuario',
+	email CHARACTER VARYING (50) NOT NULL
+		COMMENT 'Correo electrónico del usuario',
+	contrasenia CHAR(64) NOT NULL
+		COMMENT 'Contraseña del usuario',
+	hash CHAR(32) NOT NULL
+		COMMENT 'Hash único del usuario (32 caracteres)',
+	activo BOOLEAN NOT NULL DEFAULT true
+		COMMENT 'Indica si el usuario está o no activo',
+	ultimo_ingreso_fecha_hora TIMESTAMP
+		COMMENT 'Fecha y hora del último ingreso del usuario',
+	ultimo_ingreso_desde CHARACTER VARYING (45)
+		COMMENT 'Dirección IP del último ingreso del usuario',
+	ultimo_ingreso_hash CHAR(32)
+	COMMENT 'Hash del último ingreso del usuario'
 ) ENGINE = InnoDB COMMENT = 'Usuarios de la aplicación';
 ALTER TABLE usuario AUTO_INCREMENT=1000;
 CREATE UNIQUE INDEX usuario_usuario_idx ON usuario (usuario);
@@ -26,9 +38,12 @@ CREATE UNIQUE INDEX usuario_hash_idx ON usuario (hash);
 -- tabla para grupos
 DROP TABLE IF EXISTS grupo CASCADE;
 CREATE TABLE grupo (
-    id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Identificador (serial)',
-    grupo CHARACTER VARYING (30) NOT NULL COMMENT 'Nombre del grupo',
-    activo BOOLEAN NOT NULL DEFAULT true COMMENT 'Indica si el grupo se encuentra activo'
+	id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
+		COMMENT 'Identificador (serial)',
+	grupo CHARACTER VARYING (30) NOT NULL
+		COMMENT 'Nombre del grupo',
+	activo BOOLEAN NOT NULL DEFAULT true
+		COMMENT 'Indica si el grupo se encuentra activo'
 ) ENGINE = InnoDB COMMENT = 'Grupos de la aplicación';
 ALTER TABLE grupo AUTO_INCREMENT=1000;
 CREATE UNIQUE INDEX grupo_grupo_idx ON grupo (grupo);
@@ -36,40 +51,63 @@ CREATE UNIQUE INDEX grupo_grupo_idx ON grupo (grupo);
 -- tabla que relaciona usuarios con sus grupos
 DROP TABLE IF EXISTS usuario_grupo CASCADE;
 CREATE TABLE usuario_grupo (
-    usuario INTEGER NOT NULL COMMENT 'Usuario de la aplicación',
-    grupo INTEGER NOT NULL COMMENT 'Grupo al que pertenece el usuario',
-    primario BOOLEAN NOT NULL DEFAULT false COMMENT 'Indica si el grupo es el grupo primario del usuario',
-    PRIMARY KEY (usuario, grupo)
-) ENGINE = InnoDB COMMENT = 'Relación entre usuarios y los grupos a los que pertenecen';
--- ALTER TABLE usuario_grupo ADD CONSTRAINT FOREIGN KEY (usuario) REFERENCES usuario (id) ON DELETE CASCADE ON UPDATE CASCADE;
--- ALTER TABLE usuario_grupo ADD CONSTRAINT FOREIGN KEY (grupo) REFERENCES grupo (id) ON DELETE CASCADE ON UPDATE CASCADE;
+	usuario INTEGER UNSIGNED NOT NULL
+		COMMENT 'Usuario de la aplicación',
+	grupo INTEGER UNSIGNED NOT NULL
+		COMMENT 'Grupo al que pertenece el usuario',
+	primario BOOLEAN NOT NULL DEFAULT false
+		COMMENT 'Indica si el grupo es el grupo primario del usuario',
+	PRIMARY KEY (usuario, grupo),
+	CONSTRAINT usuario_grupo_usuario_fk FOREIGN KEY (usuario)
+		REFERENCES usuario (id)
+		MATCH FULL ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT usuario_grupo_grupo_fk FOREIGN KEY (grupo)
+		REFERENCES grupo (id)
+		MATCH FULL ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB COMMENT = 'Relación entre usuarios y sus grupos';
 
 -- tabla que contiene los permisos de los grupos sobre recursos
 DROP TABLE IF EXISTS auth CASCADE;
 CREATE TABLE auth (
-    id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'Identificador (serial)',
-    grupo INTEGER NOT NULL COMMENT 'Grupo al que se le concede el permiso',
-    recurso CHARACTER VARYING (300) COMMENT 'Recurso al que el grupo tiene acceso',
-    INDEX auth_grupo_idx (grupo)
+	id INTEGER UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
+		COMMENT 'Identificador (serial)',
+	grupo INTEGER UNSIGNED NOT NULL
+		COMMENT 'Grupo al que se le concede el permiso',
+	recurso CHARACTER VARYING (300)
+		COMMENT 'Recurso al que el grupo tiene acceso',
+	CONSTRAINT auth_grupo_fk FOREIGN KEY (grupo)
+		REFERENCES grupo (id)
+		MATCH FULL ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB COMMENT = 'Permisos de grupos para acceder a recursos';
--- ALTER TABLE auth ADD CONSTRAINT FOREIGN KEY (grupo) REFERENCES grupo (id) ON DELETE CASCADE ON UPDATE CASCADE;
 
--- DATOS PARA EL MÓDULO:  Sistema.Usuarios
+SET FOREIGN_KEY_CHECKS=1;
+
+-- DATOS PARA EL MÓDULO: Sistema.Usuarios
 
 INSERT INTO grupo (grupo) VALUES
-    ('sysadmin'), -- Grupo para quienes desarrollan la aplicación
-    ('appadmin')  -- Grupo para aquellos que administran la aplicación y al no ser desarrolladores no necesitan "ver todo"
+	-- Grupo para quienes desarrollan la aplicación
+	('sysadmin'),
+	-- Grupo para aquellos que administran la aplicación y al no ser 
+	-- desarrolladores no necesitan "ver todo"
+	('appadmin')  
 ;
+
 INSERT INTO auth (grupo, recurso) VALUES
-    ((SELECT id FROM grupo WHERE grupo = 'sysadmin'), '*') -- grupo sysadmin tiene acceso a todos los recursos de la aplicación
+	-- grupo sysadmin tiene acceso a todos los recursos de la aplicación
+	((SELECT id FROM grupo WHERE grupo = 'sysadmin'), '*')
 ;
 
 INSERT INTO usuario (nombre, usuario, email, contrasenia, hash) VALUES
-    -- usuario por defecto Administrador con clave admin, el hash único DEBE ser cambiado es un riesgo dejar el mismo!!!
-    ('Administrador', 'admin', 'admin@example.com', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 't7dr5B1ujphds043WMMEFWwFLeyWYqMU')
+	-- usuario por defecto admin con clave admin, el hash único DEBE ser 
+	-- cambiado, ¡¡¡ES UN RIESGO DEJAR EL MISMO!!!
+	('Administrador', 'admin', 'admin@example.com', 
+	'8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 
+	't7dr5B1ujphds043WMMEFWwFLeyWYqMU')
 ;
+
 INSERT INTO usuario_grupo (usuario, grupo, primario) VALUES
-    ((SELECT id FROM usuario WHERE usuario = 'admin'), (SELECT id FROM grupo WHERE grupo = 'sysadmin'), true)
+	((SELECT id FROM usuario WHERE usuario = 'admin'),
+		(SELECT id FROM grupo WHERE grupo = 'sysadmin'), true)
 ;
 
 COMMIT;
