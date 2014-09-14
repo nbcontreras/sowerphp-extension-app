7625ddb3acb8946779ca209974be0c1a3983b73a
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-09-14 11:58:24 -0300

    Se usa  para mostrar el tiempo que toma generar la página (sin layout)

--- View/Layouts/App.php
+++ View/Layouts/App.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!-- Design by http://www.oswd.org/design/preview/id/3459 modified by http://delaf.cl -->
+<!-- Tiempo de generación de la página (sin layout): <?=$_loadtime?> [s] -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
