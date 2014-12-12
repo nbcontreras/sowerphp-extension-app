61683873b46485f019f55030653bdf9cf516f61d
Esteban De La Fuente Rubio <esteban@delaf.cl>
2014-12-12 00:43:18 -0300

    Se mejora listado de documentación

--- View/Documentacion/index.php
+++ View/Documentacion/index.php
@@ -1,7 +1,7 @@
 <h1>Documentación</h1>
 
 <?php if ($doxygen) : ?>
-<a href="<?=$_base?>/doc/html" target="_blank" class="fright" title="Ver documentación en Doxygen">
+<a href="<?=$_base?>/doc/html" target="_blank" class="pull-right" title="Ver documentación en Doxygen">
     <img src="<?=$_base?>/img/documentacion/doxygen.png" alt="doxygen" />
 </a>
 <?php endif; ?>
@@ -12,15 +12,20 @@ enlaces($archivos);
 
 function enlaces($archivos, $ruta = '')
 {
+    $omitir = [];
     echo '<ul>',"\n";
     foreach ($archivos as $dir => $docs) {
         if (is_string($dir)) {
             $aux = $ruta.'/'.urlencode($dir);
+            if (in_array($dir, $archivos)) {
+                $omitir[] = $dir;
+                $dir = '<a href="'._BASE.'/documentacion'.$ruta.'/'.urlencode($dir).'">'.$dir.'</a>';
+            }
             echo '<li>',$dir,"\n";
             enlaces($docs, $aux);
             echo '</li>',"\n";
-        } else {
-            echo '<li><a href="',_BASE,'/documentacion',$ruta.'/'.urlencode($docs),'">',$docs,'</a></li>',"\n";
+        } else if (!in_array($docs, $omitir)) {
+            echo '<li><a href="',_BASE,'/documentacion',$ruta,'/',urlencode($docs),'">',$docs,'</a></li>',"\n";
         }
     }
     echo '</ul>',"\n";
