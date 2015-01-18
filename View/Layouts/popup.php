9be96355131789211a10a157fac744275b5901d2
Esteban De La Fuente Rubio <esteban@delaf.cl>
2015-01-18 16:29:43 -0300

    Layout popup ahora utiliza Bootstrap

--- View/Layouts/popup.php
+++ View/Layouts/popup.php
@@ -1,22 +1,31 @@
 <!DOCTYPE html>
 <html lang="es">
     <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title><?=$_header_title?></title>
         <link rel="shortcut icon" href="<?=$_base?>/img/favicon.png" />
-        <link type="text/css" href="<?=$_base?>/layouts/popup/css/screen.css" media="screen" rel="stylesheet" />
-        <link type="text/css" href="<?=$_base?>/layouts/popup/css/print.css" media="print" rel="stylesheet" />
-        <script type="text/javascript" src="<?=$_base?>/js/jquery.js"></script>
+        <link rel="stylesheet" href="<?=$_base?>/layouts/Bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="<?=$_base?>/layouts/Bootstrap/css/bootstrap-theme.min.css" />
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+            <script src="<?=$_base?>/js/html5shiv.js"></script>
+            <script src="<?=$_base?>/js/respond.js"></script>
+        <![endif]-->
+        <script src="<?=$_base?>/js/jquery.js"></script>
+        <script src="<?=$_base?>/layouts/Bootstrap/js/bootstrap.min.js"></script>
         <script type="text/javascript">
             var _url = "<?=$_url?>",
                 _base = "<?=$_base?>",
                 _request = "<?=$_request?>"
             ;
         </script>
+        <link rel="stylesheet" href="<?=$_base?>/css/font-awesome.min.css" />
 <?=$_header_extra?>
     </head>
     <body>
-        <div id="wrapper">
+        <div class="container">
 <?php echo $_content; ?>
         </div>
     </body>
