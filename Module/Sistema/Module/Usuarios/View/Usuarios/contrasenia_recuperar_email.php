01582f731f10eb02458f39126655a9f99c237619
Esteban De La Fuente Rubio <esteban@delaf.cl>
2017-01-26 15:36:24 -0300

    fix enlace recuperacion contraseña

--- Module/Sistema/Module/Usuarios/View/Usuarios/contrasenia_recuperar_email.php
+++ Module/Sistema/Module/Usuarios/View/Usuarios/contrasenia_recuperar_email.php
@@ -3,7 +3,7 @@
 Usted, o alguien más en su nombre, desde la IP <?=$ip?>, ha solicitado
 recuperar su contraseña. Para hacer esto ingrese a la siguiente dirección web:
 
-    <?=$_url?>/usuarios/contrasenia/recuperar/<?=$usuario?>/<?=$hash."\n"?>
+    <?=$_url?>/usuarios/contrasenia/recuperar/<?=urlencode($usuario)?>/<?=$hash."\n"?>
 
 Si usted no ha sido quien ha solicitado recuperar su contraseña puede ignorar
 sin problemas este correo electrónico.
