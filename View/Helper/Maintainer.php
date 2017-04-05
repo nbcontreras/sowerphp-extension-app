6afc61450af09efe66b53a5208da258b38a9d06f
Esteban De La Fuente Rubio <esteban@delaf.cl>
2017-04-05 02:44:21 -0300

    Se separa paginador de mantenedor

--- View/Helper/Maintainer.php
+++ View/Helper/Maintainer.php
@@ -26,35 +26,10 @@ namespace sowerphp\app;
 /**
  * Clase para generar los mantenedores
  * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
- * @version 2014-04-26
+ * @version 2017-04-05
  */
-class View_Helper_Maintainer extends \sowerphp\general\View_Helper_Table
+class View_Helper_Maintainer extends View_Helper_Paginator
 {
-
-    private $options; ///< Opciones del mantenedor
-    private $form; ///< Formulario (objeto de FormHelper) que se está usando en el mantenedor
-
-    /**
-     * Constructor de la clase
-     * @param options Arreglo con las opciones para el mantenedor
-     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
-     * @version 2014-04-26
-     */
-    public function __construct ($options = array(), $filter = true)
-    {
-        if ($filter) {
-            $this->options = array_merge(array(
-                'link'=>'', 'linkEnd'=>'', 'listarFilterUrl'=>'', 'thead'=>2, 'remove'=>array(2),
-            ), $options);
-        } else {
-            $this->options = array_merge(array(
-                'link'=>'', 'linkEnd'=>'', 'listarFilterUrl'=>'', 'thead'=>1, 'remove'=>array(),
-            ), $options);
-        }
-        $this->form = new \sowerphp\general\View_Helper_Form('normal');
-        $this->setExport(true);
-        $this->setExportRemove(array('rows'=>$this->options['remove'], 'cols'=>array(-1)));
-    }
 
     /**
      * Método que generará el mantenedor y listará los registros disponibles
@@ -63,7 +38,7 @@ class View_Helper_Maintainer extends \sowerphp\general\View_Helper_Table
      * @param page Página que se está revisando o 0 para no usar el paginador
      * @param create =true se agrega icono para crear registro
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
-     * @version 2016-01-17
+     * @version 2017-04-05
      */
     public function listar ($data, $pages = 1, $page = 1, $create = true)
     {
@@ -71,63 +46,17 @@ class View_Helper_Maintainer extends \sowerphp\general\View_Helper_Table
         if ($create) {
             $buffer .= '<div style="float:left"><a href="'.$this->options['link'].'/crear'.$this->options['listarFilterUrl'].'" title="Crear nuevo registro"><span class="fa fa-plus btn btn-default" aria-hidden="true"></span></a></div>'."\n";
         }
+        $this->options['link'] .= '/listar';
         if ($page)
             $buffer .= $this->paginator ($pages, $page)."\n";
-        $buffer .= parent::generate ($data, $this->options['thead']);
+        $buffer .= \sowerphp\general\View_Helper_Table::generate ($data, $this->options['thead']);
         $buffer .= $this->form->end(false)."\n";
-        if ($pages) {
-            $buffer .= '<div style="text-align:right;margin-bottom:1em;font-size:0.8em">'."\n";
-            if ($page)
-                $buffer .= '<a href="'.$this->options['link'].'/listar/0'.$this->options['linkEnd'].'">Mostrar todos los registros (sin paginar)</a>'."\n";
-            else
-                $buffer .= '<a href="'.$this->options['link'].'/listar/1'.$this->options['linkEnd'].'">Paginar registros</a>'."\n";
-            $buffer .= '</div>'."\n";
-        }
-        return $buffer;
-    }
-
-    /**
-     * Método que genera el paginador para el mantenedor
-     * @param pages Cantidad total de páginas que tienen los registros
-     * @param page Página que se está revisando o 0 para no usar el paginador
-     * @param groupOfPages De a cuantas páginas se mostrará en el paginador
-     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
-     * @version 2016-01-17
-     */
-    private function paginator ($pages, $page, $groupOfPages = 10)
-    {
-        if (!$pages)
-            return;
-        // cálculoss necesarios para crear enlaces
-        $group = ceil($page/$groupOfPages);
-        $from = ($group-1)*$groupOfPages + 1;
-        $to = min($from+$groupOfPages-1, $pages);
-        // crear enlaces para paginador
-        $buffer = '<div style="float:left;margin-left:20%;width:50%;text-align:center"><nav><ul class="pagination">'."\n";
-        if ($page==1)
-            $buffer .= '<li class="disabled"><span class="fa fa-fast-backward" aria-hidden="true"></span></li>';
+        $buffer .= '<div style="text-align:right;margin-bottom:1em;font-size:0.8em">'."\n";
+        if ($page)
+            $buffer .= '<a href="'.$this->options['link'].'/0'.$this->options['linkEnd'].'">Mostrar todos los registros (sin paginar)</a>'."\n";
         else
-            $buffer .= '<li><a href="'.$this->options['link'].'/listar/1'.$this->options['linkEnd'].'" title="Ir a la primera página"><span class="fa fa-fast-backward" aria-hidden="true"></span></a></li>';
-        if ($group==1)
-            $buffer .= '<li class="disabled"><span class="fa fa-backward" aria-hidden="true"></span></li>';
-        else
-            $buffer .= '<li><a href="'.$this->options['link'].'/listar/'.($from-1).$this->options['linkEnd'].'" title="Ir al grupo de páginas anterior (página '.($from-1).')"><span class="fa fa-backward" aria-hidden="true"></span></a></li>';
-        for ($i=$from; $i<=$to; $i++) {
-            if ($page==$i)
-                $buffer .= '<li class="active"><a href="#" onclick="return false">'.$i.'</a></li>';
-            else
-                $buffer .= '<li><a href="'.$this->options['link'].'/listar/'.$i.$this->options['linkEnd'].'" title="Ir a la página '.$i.'">'.$i.'</a></li>';
-        }
-        if ($group==ceil($pages/$groupOfPages))
-            $buffer .= '<li class="disabled"><span class="fa fa-forward" aria-hidden="true"></span></li>';
-        else
-            $buffer .= '<li><a href="'.$this->options['link'].'/listar/'.($to+1).$this->options['linkEnd'].'" title="Ir al grupo de páginas siguiente (página '.($to+1).')"><span class="fa fa-forward" aria-hidden="true"></span></a></li>';
-        if ($page==$pages)
-            $buffer .= '<li class="disabled"><span class="fa fa-fast-forward" aria-hidden="true"></span></li>';
-        else
-            $buffer .= '<li><a href="'.$this->options['link'].'/listar/'.$pages.$this->options['linkEnd'].'" title="Ir a la última página"><span class="fa fa-fast-forward" aria-hidden="true"></span></a></li>';
-        $buffer .= '</ul></nav></div>'."\n";
-        // retornar enlaces
+            $buffer .= '<a href="'.$this->options['link'].'/1'.$this->options['linkEnd'].'">Paginar registros</a>'."\n";
+        $buffer .= '</div>'."\n";
         return $buffer;
     }
 
