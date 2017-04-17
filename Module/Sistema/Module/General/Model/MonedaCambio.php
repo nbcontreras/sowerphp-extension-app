508faff557f4ac9ece0c1fe3aa7300a6ebc59bdf
Esteban De La Fuente Rubio <esteban@delaf.cl>
2017-04-17 11:40:12 -0300

    fix cambio automatico

--- Module/Sistema/Module/General/Model/MonedaCambio.php
+++ Module/Sistema/Module/General/Model/MonedaCambio.php
@@ -127,7 +127,7 @@ class Model_MonedaCambio extends \Model_App
             $a = strtoupper($a);
             parent::__construct($desde, $a, $fecha);
             // si no existe el tipo de cambio, buscar si existe "a" USD y luego desde USD a la moneda $a original
-            /*if (!$this->valor) {
+            if (!$this->valor and $a!='USD') {
                 $MonedaCambioUSD = (new Model_MonedaCambios)->get($desde, 'USD', $fecha);
                 if ($MonedaCambioUSD->valor) {
                     $USD = (new Model_MonedaCambios)->get('USD', $a, $fecha);
@@ -136,7 +136,7 @@ class Model_MonedaCambio extends \Model_App
                         $this->save();
                     }
                 }
-            }*/
+            }
         } else {
             parent::__construct();
         }
