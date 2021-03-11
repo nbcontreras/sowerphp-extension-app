91c9982cb16ec42cb89fb1d310c5df1cfe7d06d0
Esteban De La Fuente Rubio <esteban@delaf.cl>
2021-03-11 17:07:02 -0300

    Se quita documentación innecesaria que hoy está disponible en otras vías

--- View/Documentacion/index.php
+++ View/Documentacion/index.php
@@ -1,15 +1,6 @@
 <div class="page-header"><h1>Documentación</h1></div>
-
-<?php if ($doxygen) : ?>
-<a href="<?=$_base?>/doc/html/" target="_blank" class="pull-right" title="Ver documentación en Doxygen">
-    <img src="<?=$_base?>/img/documentacion/doxygen.png" alt="doxygen" />
-</a>
-<?php endif; ?>
-
 <?php
-
 enlaces($archivos);
-
 function enlaces($archivos, $ruta = '')
 {
     $omitir = [];
